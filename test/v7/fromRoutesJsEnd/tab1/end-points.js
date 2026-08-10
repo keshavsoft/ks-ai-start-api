1431263f5b83f86bd2879f255290180f8a43d164
keshavsoft <nknnkumar@yahoo.com>
2026-08-10 09:26:14 +0530

    8

--- test/v7/fromRoutesJsEnd/tab1/end-points.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const tableName = "tab1.json";
-const tablePath = "Data/tab1.json";
-const configPath = "Config/Schemas/tab1.json";
-
-const router = express.Router();
-
-export { router };
